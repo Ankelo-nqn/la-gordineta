5160f7e1f77ebc92fe88683b859650f9e545ef8e
Quinoto <angelcellillo@gmail.com>
2022-12-30 17:04:11 -0300

    arrancamos personalizacion fuera del tp

--- index.php
+++ index.php
@@ -14,14 +14,34 @@
     menu();
     $mensaje = $_GET['mensaje'];
   ?>  
+  <script type="text/javascript">
+    function mensaje(){
+      alert("Un mensaje de prueba");
+    }
+  </script>
 </head>
 
 <body>
-  <br><br>
-  <h1 class="text-center">La Gordineta Neuquina</h1>
-  <h2 class="text-center">Control de gastos</h2>
-  <h3 class="text-center">Inicio</h3>
-  <br><br>
+  <!-- Columna Izquierda -->
+  <div class="col-6"></div>
+
+  <!-- Cuerpo -->
+  <div class="col-6">
+    <br><br>
+    <h1 class="text-center">La Gordineta Neuquina</h1>
+    <h2 class="text-center">Control de gastos</h2>
+    <h3 class="text-center">Inicio</h3>
+    <br><br>
+    <div class="d-grid gap-2 d-md-flex justify-content-md-end">
+      <button class="btn btn-primary me-md-2" type="button" onclick="js/mensaje.js" >Externo</button>
+      <button class="btn btn-primary" type="button" onclick="mensaje()">Mensaje</button>
+    </div>
+  </div>
+
+  <!-- Columna Derecha -->
+  <div class="col-3"></div>
+
+  <!-- Footer -->
   <?php echo "<br><h3 class='text-center'>$mensaje</h3>" ?>
 </body>
 </html>
